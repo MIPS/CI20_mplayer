MIPS/CI20_mplayer

commit db129a713f8525b19d838c2479472c912ded2b38

modify the SIGSEGV bug of continuous player
Change-Id: Id4e9cedfeb662f22480d8a9060283f753ca9ae39

libjzipu/jz4780_ipu_hal/dmmu.c
@@ -281,7 +281,7 @@ int dmmu_match_user_mem_tlb(void * vaddr, int size)
     int pn = size/PAGE_SIZE;
     //ALOGD("pn=%d", pn);
     int pg;
-    for(pg=0; pg<pn; pg++ ) {
+    for(pg=0; pg<(pn + 1); pg++ ) {
         //ALOGD("pg=%d pc=%p", pg, pc);
         volatile unsigned char c = *( volatile unsigned char*)pc;
         pc += PAGE_SIZE;

libjzipu/jz4780_ipu.c
@@ -113,10 +113,14 @@ static int closeIPU(void)
 	mIPU_inited = 0;
 	printf("closeIPU mIPUHandler=%p\n", mIPUHandler);
 
-	munmap(fb_vaddr, fbSize);
-	close(fbfd);
+
 	free(tmp_rgb_vaddr);
 	tmp_rgb_vaddr = NULL;
+
+	munmap(fb_vaddr, fbSize);
+
+	close(fbfd);
+	fbfd = -1;
 	
 	return 0;
 }
@@ -414,9 +418,13 @@ static int updateVideo(struct vf_instance *vf, mp_image_t *mpi, double pts)
 		initIPUDestBuffer(vf->posx, vf->posy, vf->priv->ctx->dstW, vf->priv->ctx->dstH);
 		initIPUSourceBuffer(vf, mpi, pts);
 #ifdef USE_JZ4780_DMMU
-		mapIPUSourceBuffer(vf, mpi);
+		if (mapIPUSourceBuffer(vf, mpi) < 0) {
+			return -1;
+		}
 #ifndef JZ4780_IPU_LCDC_OVERLAY
-		mapIPUDestBuffer(fb_var_info.xres, fb_var_info.yres, 4, fb_vaddr);
+		if (mapIPUDestBuffer(fb_var_info.xres, fb_var_info.yres, 4, fb_vaddr) < 0) {
+			return -1;
+		}
 #endif
 #endif
 		
@@ -437,7 +445,9 @@ static int updateVideo(struct vf_instance *vf, mp_image_t *mpi, double pts)
 		//printf("per frame initIPUSourceBuffer"); 
 		initIPUSourceBuffer(vf, mpi, pts);
 #ifdef USE_JZ4780_DMMU
-		mapIPUSourceBuffer(vf, mpi);
+		if (mapIPUSourceBuffer(vf, mpi) < 0) {
+			return -1;
+		}
 #endif
 	}
 

commit 0d16e427a2c4d8a0d0804416f57c7c5fb6de0a48

Fix possible segfault in dmmu_match_user_mem_tlb()
This was introduced by commit db129a71. If the size passed to
dmmu_match_user_mem_tlb() is an exact multiple of the page size, the
code may segfault as it attempts to read off the end of the buffer.
Changed to round up the size given to the page size instead, which
should have the same effect intended by the original change, while
fixing the case of size being a multiple of PAGE_SIZE.

Signed-off-by: Alex Smith <alex.smith@imgtec.com>

libjzipu/jz4780_ipu_hal/dmmu.c
@@ -278,10 +278,10 @@ int dmmu_match_user_mem_tlb(void * vaddr, int size)
         return 1;
     volatile unsigned char * pc;
     pc = (unsigned char *)vaddr;
-    int pn = size/PAGE_SIZE;
+    int pn = ((size + (PAGE_SIZE-1)) & ~(PAGE_SIZE-1))/PAGE_SIZE;
     //ALOGD("pn=%d", pn);
     int pg;
-    for(pg=0; pg<(pn + 1); pg++ ) {
+    for(pg=0; pg<pn; pg++ ) {
         //ALOGD("pg=%d pc=%p", pg, pc);
         volatile unsigned char c = *( volatile unsigned char*)pc;
         pc += PAGE_SIZE;