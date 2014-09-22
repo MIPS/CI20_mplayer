28d993aaf6f12225225b30ccc75ff5af85b70471
llcheng <llcheng@ingenic.cn>
2014-09-22 15:53:01 +0800

    Add the title of Ingenic Semiconductor CO.,LTD in the related files.

--- libjzipu/jz4780_ipu_hal/jz_ipu.h
+++ libjzipu/jz4780_ipu_hal/jz_ipu.h
@@ -1,14 +1,22 @@
-/* hardware/xb4770/libjzipu/android_jz4780_ipu.h
+/*
+ * Copyright (C) 2006-2014 Ingenic Semiconductor CO.,LTD.
  *
- * Copyright (c) 2012 Ingenic Semiconductor Co., Ltd.
- *              http://www.ingenic.com/
+ * This file is part of MPlayer.
  *
- * Hal head file for Ingenic IPU device
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
-*/
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #ifndef __ANDROID_JZ4780_IPU_H__
 #define __ANDROID_JZ4780_IPU_H__
--- libjzipu/jz4780_ipu_hal/jz_ipu_table_const.c
+++ libjzipu/jz4780_ipu_hal/jz_ipu_table_const.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2006-2014 Ingenic Semiconductor CO.,LTD.
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include "jz_ipu.h"
 struct host_ipu_table hostiputable = {{647},{
 	{1.000000,  1,  1},{0.500000,  1,  2},{0.333333,  1,  3},{0.250000,  1,  4},
